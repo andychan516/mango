16bfc7fc6079141314fbea88d26875a227871679
andychan516 <623247195@qq.com>
2021-05-04 21:36:14 +0800

    *modify project name

---
+++ src/main/java/com/louis/mango/admin/controller/SysUserController.java
@@ -0,0 +1,25 @@
+package com.louis.mango.admin.controller;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.louis.mango.admin.service.SysUserService;
+
+@RestController
+@RequestMapping("user")
+public class SysUserController {
+    Logger logger = LogManager.getLogger(SysUserController.class);
+
+    @Autowired
+    private SysUserService sysUserService;
+    
+    @GetMapping(value="/findAll")
+    public Object findAll() {
+        logger.info("hehe");
+        return sysUserService.findAll();
+    }
+}
